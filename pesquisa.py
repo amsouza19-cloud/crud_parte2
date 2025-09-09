c6c36cf295c191ff93018653bfc9259eb6414bb9
amsouza19-cloud <am.souza19@gmail.com>
2025-09-09 11:44:18 +0000

    Primeiro commit

--- pesquisa.py
+++ pesquisa.py
@@ -1,1 +1,10 @@
+def ver_livros_disponiveis(lista_livros):
+    if not lista_livros:
+        print("Nenhum livro disponível no momento.\n")
+        return
 
+    print("--------------------------------------------------")
+    print("LIVROS DISPONÍVEIS PARA LEITURA")
+    for livro in lista_livros:
+        print(f"{livro['id']} - {livro['titulo']} ({livro['ano']}) - Autor: {livro['autor']} - Gênero: {livro['genero']}")
+    print("--------------------------------------------------\n")
